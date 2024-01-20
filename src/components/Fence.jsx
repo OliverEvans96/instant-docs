621cd0144e81f9400baed91b2b4d0f8a207ef9ff
Joe Averbukh <joeaverbukh@gmail.com>
2024-01-19 22:13:08 -0500

    Add copy to Fence

--- src/components/Fence.jsx
+++ src/components/Fence.jsx
@@ -1,7 +1,13 @@
 import { Fragment } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { ClipboardCopyIcon } from '@heroicons/react/outline';
 import Highlight, { defaultProps } from 'prism-react-renderer'
+import { useState } from 'react';
 
-export function Fence({ children, language }) {
+
+export function Fence({ children, language, showCopy }) {
+  const [copyLabel, setCopyLabel] = useState('Copy')
+  console.log("showCopy", showCopy);
   return (
     <Highlight
       {...defaultProps}
@@ -10,21 +16,42 @@ export function Fence({ children, language }) {
       theme={undefined}
     >
       {({ className, style, tokens, getTokenProps }) => (
-        <pre className={className} style={style}>
-          <code>
-            {tokens.map((line, lineIndex) => (
-              <Fragment key={lineIndex}>
-                {line
-                  .filter((token) => !token.empty)
-                  .map((token, tokenIndex) => (
-                    <span key={tokenIndex} {...getTokenProps({ token })} />
-                  ))}
-                {'\n'}
-              </Fragment>
-            ))}
-          </code>
-        </pre>
-      )}
-    </Highlight>
+        <div>
+          {showCopy &&
+            <div className="flex justify-end">
+              <CopyToClipboard text={children}>
+                <button
+                  onClick={() => {
+                    setCopyLabel('Copied!')
+                    setTimeout(() => {
+                      setCopyLabel('Copy')
+                    }, 2000)
+                  }}
+                  className="flex items-center gap-x-1
+            rounded-md bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  <ClipboardCopyIcon className="-ml-0.5 h-4 w-4" aria-hidden="true" />{copyLabel}</button>
+              </CopyToClipboard>
+            </div>
+          }
+          <pre className={className} style={style}>
+            <code>
+              {tokens.map((line, lineIndex) => (
+                <Fragment key={lineIndex}>
+                  {line
+                    .filter((token) => !token.empty)
+                    .map((token, tokenIndex) => (
+                      <span key={tokenIndex} {...getTokenProps({ token })} />
+                    ))}
+                  {'\n'}
+                </Fragment>
+              ))}
+            </code>
+          </pre>
+        </div>
+
+      )
+      }
+    </Highlight >
   )
 }
