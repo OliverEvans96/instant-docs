87a11650c506175fa916242eb73bf9d56bd3b8fa
Joe Averbukh <joeaverbukh@gmail.com>
2024-03-05 11:46:47 -0500

    rm logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,8 @@
-import Image from 'next/future/image'
-
-import InstantLogo from '@/images/playful_sun.svg'
-
-export function LogoImage(props) {
-  return <Image src={InstantLogo} alt="Instant Logo" width={27} {...props} />;
-}
-
 export function Logo() {
   return (
     <span className="inline-flex space-x-2">
-      <LogoImage />
       <div>
-        <span className="font-mono text-lg font-bold lowercase dark:text-white">
+        <span className="font-mono text-lg lowercase dark:text-white">
           Instant
         </span>
       </div>
