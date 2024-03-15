954d1745bfeb5144229394cd3c25e8a444fc5648
Joe Averbukh <joeaverbukh@gmail.com>
2024-03-15 14:03:00 -0400

    Add info callout

--- markdoc/tags.js
+++ markdoc/tags.js
@@ -8,7 +8,7 @@ const tags = {
       type: {
         type: String,
         default: 'note',
-        matches: ['note', 'warning'],
+        matches: ['info', 'note', 'warning'],
         errorLevel: 'critical',
       },
     },
